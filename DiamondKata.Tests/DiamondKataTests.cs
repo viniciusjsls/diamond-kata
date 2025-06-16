61b66f9e8694e38cf94fdb686ac6d1d4bd675c9d
Vinicius Sacramento <viniciusjose57@gmail.com>
2025-06-16 20:26:44 +0100

    add first tests guidance for input validation

---
+++ DiamondKata.Tests/DiamondKataTests.cs
@@ -0,0 +1,28 @@
+namespace DiamondKata.Tests;
+
+public class DiamondKataTests
+{
+    [Fact]
+    public void GivenInputCharLowerThanA_ThenThrowException()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    [Fact]
+    public void GivenInputCharBiggerThanA_ThenThrowException()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    [Fact]
+    public void GivenInputCharBetweenAAndZ_ThenNoException()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+}
